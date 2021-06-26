625c4bf1af0560e7870db15130469e4b50d1aa28
Undercover Stamp <byemc@outlook.com>
2021-06-26 20:19:25 +0100

    fix the build

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gb-run.o gb-run.c
+md tmp
+cd tmp
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gb-run.o ..\gb-run.c
 C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o gb-run.gb gb-run.o
-pause
+copy gb-run.gb ..\gb-run.gb
+cd ..
+del /Q tmp\*
+echo This is filler, so rmdir works
+rmdir tmp
