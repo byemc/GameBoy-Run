11e3c4751cb888bc0745917dc9ff72a85246f3ff
Undercover Stamp <byemc@outlook.com>
2021-06-26 19:38:42 +0100

    starting docs, also learning how C works.
    Ignore those.
    
    Please.

---
+++ gb-run-client.py
@@ -0,0 +1,27 @@
+#!/usr/var/bin python3
+
+# GameBoy-Run Desktop-side client
+# Written by ByeMC
+#
+# Contributors: 
+# ByeMC
+# [[ ADD YOUR NAME HERE, IF YOU CONRIBUTED TO THIS FILE ]]
+
+
+# Importing libraries
+import tkinter as tk # for GUI
+import logging as log # for logging
+log.debug('Imported libs.')
+
+# Setup
+
+# Main Loop
+
+
+
+
+
+
+
+# THIS SHOULD ONLY RUN AT THE END OF THE SCRIPT
+log.debug('Script finished.')
